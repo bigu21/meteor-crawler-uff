e46f2507cc89cf3e921b11cd0b0db94f7621b584
Gabriel Almeida <gabriel.almeida.rj@gmail.com>
2014-04-20 20:57:31 +0000

    Changed infos from UFRJ to UFF

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Stealer UFRJ
+# Stealer UFF
 
-A __Meteor__ package to crawl data from http://intranet.ufrj.br and SIGA, 
-both internal student management online environment of Federal University of Rio de Janeiro(UFRJ)
+A __Meteor__ package to crawl data from https://sistemas.uff.br/iduff and idUFF, 
+both internal student management online environment of Federal Fluminense University(UFF)
 
 This is intended for internal use as a Meteor package submoduled at [Caroninha](http://github.com/gabrielalmeida/caroninha)
--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
-    summary: "Stealer UFRJ crawls data from UFRJ Intranet and SIGA"
+    summary: "Stealer UFF crawls data from idUFF"
 });
 
 Package.on_use(function(api) {
 
     api.use(['underscore', 'http-more' ], ['client', 'server']);
-    api.add_files('stealer-ufrj.js', ['client', 'server']);
+    api.add_files('stealer-uff.js', ['client', 'server']);
 });
 
 
 Package.on_test(function(api) {
-    api.use(['tinytest', 'http-more', 'stealer-ufrj'], ['client', 'server']);
-    api.add_files(['stealer-ufrj-tests.js'], ['client', 'server']);
+    api.use(['tinytest', 'http-more', 'stealer-uff'], ['client', 'server']);
+    api.add_files(['stealer-uff-tests.js'], ['client', 'server']);
 });
