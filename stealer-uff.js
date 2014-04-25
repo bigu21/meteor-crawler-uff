54668891ab53054c4766bb1f32ecb53c03d5fbf2
Gabriel Almeida <gabriel.almeida.rj@gmail.com>
2014-04-25 00:45:23 +0000

    idUFF is a chaos

--- stealer-uff.js
+++ stealer-uff.js
@@ -1,49 +1,59 @@
 if(Meteor.isServer) {
     var cheerio =  Meteor.require('cheerio');
-    console.log("Server");
+    var rest =  Meteor.require('restler');
+    //var request =  Meteor.require('superagent');
+    //var request =  Meteor.require('request');
 
-     login_ufrj= function(settings) {
-        console.log("Stealer");
+    //var jar = request.jar();
 
-        // Default METHOD
-        if(!settings.method)
-            settings.method = 'GET';
+    //request({
+        //method: 'POST',
+        //uri: 'https://sistemas.uff.br/iduff/sid137avUfd98/login.uff',
+        //form: {
+            //'login': 'login',
+            //'login:btnLogar': 'Logar',
+            //'login:id': "***REMOVED***",
+            //'login:senha': '***REMOVED***',
+            //'javax.faces.ViewState': 'j_id1'
+        //},
+        //jar: jar,
+        //followRedirects: true,
+        //followAllRedirects: true,
+        //strictSLL: false,
+        //rejectUnauthorized: false,
+        //headers: {
+            //'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36',
+            //'Accept': '/',
+            //'Accept-Encoding': 'gzip,deflate,sdch',
+            //'Accept-Language': 'en-US,en;q=0.8,pt;q=0.6',
+        //}
+    //}, function(err, res, body) {
+        //console.log(jar);
+    //});
 
-        if(!settings.url)
-            throw new Meteor.Error(500, "An URL must be given.");
 
-        content = HTTP.call(settings.method, settings.url, settings.options).content;
+    //request
+        //.post('https://sistemas.uff.br/iduff/sid137avUfd98/login.uff')
+        //.type('form')
+        //.set('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36')
+        //.set('Accept', '/')
+        //.set('Accept-Encoding', 'gzip,deflate,sdch')
+        //.set('Accept-Language', 'en-US,en;q=0.8,pt;q=0.6')
+        //.send({
+            //'login': 'login',
+            //'login:btnLogar': 'Logar',
+            //'login:id': "***REMOVED***",
+            //'login:senha': '***REMOVED***',
+            //'javax.faces.ViewState': 'j_id1'
+        //})
+        //.end(function(error, res) {
+            //console.log(res);
+        //});
 
-        $ = cheerio.load(content);
-
-        console.log($("html").html());
-    }
-
-    settings =  {
-        url: 'https://intranet.ufrj.br/Utilidades2006/Login.asp',
-        options: {
-            followAllRedirects: true,
-            strictSSL: false,
-            jar: true,
-            params: {
-                usuario: "***REMOVED***",
-                senha: '***REMOVED***'
-            }
-        }
-    }
-
-    login_ufrj(settings);
-
-    Meteor.methods({
-        login_ufrj: function(username, password) {
-            _.extend(settings,{username: username, password: password});
-            login_ufrj(settings)
-        }
-    });
+    rest.post('https://sistemas.uff.br/iduff/sid137avUfd98/login.uff', {
+    }).on('complete', function(result, response) {
+        console.log(result);
+        //console.log(response);
+   });
 }
 
-if(Meteor.isClient) {
-    Meteor.call('login_ufrj', '***REMOVED***', '***REMOVED***', function(e,r) {
-
-    });
-}
