1489d8a5c60da1317ea0c0a1bc317e65dd2dbf61
Gabriel Almeida <gabriel.almeida.rj@gmail.com>
2014-06-06 06:31:54 +0000

    http-more package removed

--- package.js
+++ package.js
@@ -4,12 +4,12 @@ Package.describe({
 
 Package.on_use(function(api) {
 
-    api.use(['underscore', 'http-more' ], ['client', 'server']);
+    api.use(['underscore'], ['client', 'server']);
     api.add_files('stealer-uff.js', ['client', 'server']);
 });
 
 
 Package.on_test(function(api) {
-    api.use(['tinytest', 'http-more', 'stealer-uff'], ['client', 'server']);
+    api.use(['tinytest', 'stealer-uff'], ['client', 'server']);
     api.add_files(['stealer-uff-tests.js'], ['client', 'server']);
 });
